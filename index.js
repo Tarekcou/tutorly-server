17a6580bf49b921aeae3de222cc05b8ade1a9a62
tarek <sitarek77@gmail.com>
2024-12-26 00:09:53 +0600

    2 collection

--- index.js
+++ index.js
@@ -28,93 +28,144 @@ async function run() {
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     const database = client.db("tutorDB");
-    const productCollection = database.collection("tutor");
-    app.get("/products", async (req, res) => {
-      const cursor = productCollection.find();
+    const tutorCollection = database.collection("tutor");
+    const bookedTutorCollection = database.collection("bookedTutor");
+    app.get("/tutorials", async (req, res) => {
+      const cursor = tutorCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
-    app.get("/category/:categoryName", async (req, res) => {
-      const categoryName = req.params.categoryName;
-      const query = { categoryName: categoryName };
+    app.get("/tutorials/:language", async (req, res) => {
+      const language = req.params.language;
+      // console.log("language: " + language);
+      const query = { language: language };
       const option = { upsert: false };
-      const cursor = productCollection.find(query, option);
-      // (cursor);
+      const cursor = tutorCollection.find(query, option);
+      // console.log(cursor);
       const result = await cursor.toArray();
-      // (result);
+      // console.log(result);
       res.send(result);
     });
     app.get("/cartItem/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const option = { upsert: false };
-      const cursor = productCollection.find(query, option);
+      const cursor = tutorCollection.find(query, option);
       // (cursor);
       const result = await cursor.toArray();
       result;
       res.send(result);
     });
-    app.get("/myEquipment/:myEmail", async (req, res) => {
+    app.get("/myTutorials/:myEmail", async (req, res) => {
       const myEmail = req.params.myEmail;
       const query = { email: myEmail };
-      const cursor = productCollection.find(query);
+      const cursor = tutorCollection.find(query);
+      const result = await cursor.toArray();
+      console.log(result);
+      res.send(result);
+    });
+    app.get("/booked-tutors/:myEmail", async (req, res) => {
+      const myEmail = req.params.myEmail;
+      const query = { email: myEmail };
+      const cursor = bookedTutorCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
     app.get("/products/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await productCollection.findOne(query);
+      const result = await tutorCollection.findOne(query);
 
       res.send(result);
     });
-    app.post("/add-product", async (req, res) => {
+    app.post("/add-tutorials", async (req, res) => {
       const product = req.body;
       // (product);
-      const result = await productCollection.insertOne(product);
+      const result = await tutorCollection.insertOne(product);
       res.send(result);
     });
-    app.delete("/product/:id", async (req, res) => {
+
+    app.post("/add-booked-tutorials", async (req, res) => {
+      const tutorial = req.body;
+      // (product);
+      const result = await bookedTutorCollection.insertOne(tutorial);
+      // const id = tutorial.tutorId;
+      // const query = { _id: new ObjectId(id) };
+      // const tutor = tutorCollection.findOne(query);
+      // const newCount = 0;
+      // if (tutor.tutorCount) {
+      //   newCount = tutor.tutorCount + 1;
+      // } else {
+      //   newCount = 1;
+      // }
+      // // now update tutorCollection
+      // const filter = { _id: new ObjectId(id) };
+      // const updateTutor = {
+      //   $set: {
+      //     tutorCount: newCount,
+      //   },
+      // };
+      // const updateResult = await tutorCollection.updateOne(filter, updateTutor);
+      res.send(result);
+    });
+
+    // Increment the review count for a specific tutor
+    app.post("/review/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await productCollection.deleteOne(query);
+      const tutor = await tutorCollection.findOne(query);
+      // console.log(id, tutor);
+      let newCount = 0;
+      if (tutor.review) {
+        newCount = parseInt(tutor.review) + 1;
+        console.log(newCount);
+      } else {
+        newCount = 1;
+      }
+      // now update tutorCollection
+      const filter = { _id: new ObjectId(id) };
+      const updateTutor = {
+        $set: {
+          review: newCount,
+        },
+      };
+      // console.log(updateTutor);
+      const updateResult = await tutorCollection.updateOne(filter, updateTutor);
+      console.log(updateResult);
+    });
+    app.delete("/tutorials/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await tutorCollection.deleteOne(query);
       // console.log(result);
       res.send(result);
     });
     // Update product
-    app.put("/product/:id", async (req, res) => {
+    app.put("/tutorials/:id", async (req, res) => {
       const id = req.params.id;
-      // (id);
-
+      // console.log(id);
       const query = { _id: new ObjectId(id) };
       const option = { upsert: false };
-      const updateProduct = req.body;
-      // (updateProduct);
-      const newProduct = {
+      const updateTutorials = req.body;
+      // console.log(updateTutorials);
+      const newTutorials = {
         $set: {
-          imageUrl: updateProduct.imageUrl,
-          itemName: updateProduct.itemName,
-          categoryName: updateProduct.categoryName,
-          description: updateProduct.description,
-          originalPrice: updateProduct.originalPrice,
-          discountedPrice: updateProduct.discountedPrice,
-          discountPercentage: updateProduct.discountPercentage,
+          image: updateTutorials.image,
+          country: updateTutorials.country,
+          rating: updateTutorials.rating,
+          price: updateTutorials.price,
+          language: updateTutorials.language,
 
-          rating: updateProduct.rating,
-          customization: updateProduct.customization,
-          processingTime: updateProduct.processingTime,
-          stockStatus: updateProduct.stockStatus,
-          stockItem: updateProduct.stockItem,
+          description: updateTutorials.description,
         },
       };
-      newProduct;
 
-      const result = await productCollection.updateOne(
+      const result = await tutorCollection.updateOne(
         query,
-        newProduct,
+        newTutorials,
         option
       );
-      // (result);
+      console.log(result);
       res.send(result);
     });
     console.log(
