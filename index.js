1e9a142087650a2a6b3775b72d6173043c32dfc6
tarek <sitarek77@gmail.com>
2025-02-18 08:35:09 +0600

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const express = require("express");
 const cors = require("cors");
 const ck = require("ckey");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const ports = process.env.PORT || 5005;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://online-tutor-2c707.firebaseapp.com",
+    ],
+    credentials: true,
+  }))
+// app.use(cors());
+
 app.use(express.json());
+app.use(cookieParser());
+const verifyToken = (req, res, next) => {
+  const token = req.cookie?.token;
+  console.log(token);
+  if (!token) return res.status(401).send({ message: "unauthorized access" });
+
+  jwt.verify(token, ck.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) return res.status(401).send({ message: "invalid token" });
+    req.user = decoded;
+    next();
+  });
+};
 // console.log(ck.DB_USER + ": " + ck.DB_PASS);
 
 const uri = `mongodb+srv://${ck.DB_USER}:${ck.DB_PASS}@cluster0.ufb0a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -30,6 +53,31 @@ async function run() {
     const database = client.db("tutorDB");
     const tutorCollection = database.collection("tutor");
     const bookedTutorCollection = database.collection("bookedTutor");
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      // console.log(user);
+      // console.log(ck.ACCESS_TOKEN_SECRET);
+      const token = jwt.sign(user, ck.ACCESS_TOKEN_SECRET, { expiresIn: "5h" });
+      // console.log(token);
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+          // expires: new Date(Date.now() + 5*60*100s0) });
+        })
+        .send({ success: true });
+      // return res.status(200).json({ success: true });
+    });
+    app.post("/logout", (req, res) => {
+      res
+        .clearCookie("token", {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
+
     app.get("/tutorials", async (req, res) => {
       const cursor = tutorCollection.find();
       const result = await cursor.toArray();
@@ -59,6 +107,10 @@ async function run() {
     app.get("/myTutorials/:myEmail", async (req, res) => {
       const myEmail = req.params.myEmail;
       const query = { email: myEmail };
+      console.log(req);
+      // if (req.user.email !== req.query.email) {
+      //   return res.status(403).send({ message: "forbidded  access" });
+      // }
       const cursor = tutorCollection.find(query);
       const result = await cursor.toArray();
       console.log(result);
@@ -112,7 +164,11 @@ async function run() {
     // Increment the review count for a specific tutor
     app.post("/review/:id", async (req, res) => {
       const id = req.params.id;
+      const id2 = req.body;
+      // console.log(id, id2.reviewId);
       const query = { _id: new ObjectId(id) };
+      // console.log(query2);
+
       const tutor = await tutorCollection.findOne(query);
       // console.log(id, tutor);
       let newCount = 0;
@@ -124,6 +180,9 @@ async function run() {
       }
       // now update tutorCollection
       const filter = { _id: new ObjectId(id) };
+      const filter2 = { _id: new ObjectId(id2.reviewId) };
+
+      console.log(filter, filter2);
       const updateTutor = {
         $set: {
           review: newCount,
@@ -131,6 +190,10 @@ async function run() {
       };
       // console.log(updateTutor);
       const updateResult = await tutorCollection.updateOne(filter, updateTutor);
+      const updateResult2 = await bookedTutorCollection.updateOne(
+        filter2,
+        updateTutor
+      );
       console.log(updateResult);
     });
     app.delete("/tutorials/:id", async (req, res) => {
